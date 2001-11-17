970fd7ecc335920afdc38e72abc80b6bf2c0f5ac
Yonatan Feldman <yon@milliped.com>
2001-11-17 21:56:07 +0000

    update

--- sql/oracle/faq-portlet-create.sql
+++ sql/oracle/faq-portlet-create.sql
@@ -19,20 +19,38 @@ declare
 begin
   ds_id := portal_datasource.new(
     name             => 'faq-portlet',
-    link             => 'faq',
-    description      => 'Displays the given folder_id ',
-    content	     => 'faq_portlet::show',
-    configurable_p   => 't'
+    description      => 'Displays the given folder_id'
   );
 
-  -- package_id must be configured
+  --  the standard 4 params
+
+  -- shadeable_p 
   portal_datasource.set_def_param (
 	datasource_id => ds_id,
 	config_required_p => 't',
-	configured_p => 'f',
-	key => 'package_id',
-	value => ''
-);
+	configured_p => 't',
+	key => 'shadeable_p',
+	value => 't'
+);	
+
+
+  -- hideable_p 
+  portal_datasource.set_def_param (
+	datasource_id => ds_id,
+	config_required_p => 't',
+	configured_p => 't',
+	key => 'hideable_p',
+	value => 't'
+);	
+
+  -- user_editable_p 
+  portal_datasource.set_def_param (
+	datasource_id => ds_id,
+	config_required_p => 't',
+	configured_p => 't',
+	key => 'user_editable_p',
+	value => 'f'
+);	
 
   -- shaded_p 
   portal_datasource.set_def_param (
@@ -43,6 +61,125 @@ begin
 	value => 'f'
 );	
 
+  -- package_id must be configured
+  portal_datasource.set_def_param (
+	datasource_id => ds_id,
+	config_required_p => 't',
+	configured_p => 'f',
+	key => 'package_id',
+	value => ''
+);
+
+
+end;
+/
+show errors
+
+
+declare
+	foo integer;
+begin
+	-- create the implementation
+	foo := acs_sc_impl.new (
+		'portal_datasource',
+		'faq_portlet',
+		'faq_portlet'
+	);
+
+end;
+/
+show errors
+
+declare
+	foo integer;
+begin
+
+	-- add all the hooks
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'MyName',
+	       'faq_portlet::my_name',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'GetPrettyName',
+	       'faq_portlet::get_pretty_name',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'Link',
+	       'faq_portlet::link',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'AddSelfToPage',
+	       'faq_portlet::add_self_to_page',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'Show',
+	       'faq_portlet::show',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'Edit',
+	       'faq_portlet::edit',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'RemoveSelfFromPage',
+	       'faq_portlet::remove_self_from_page',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'MakeSelfAvailable',
+	       'faq_portlet::make_self_available',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'faq_portlet',
+	       'MakeSelfUnavailable',
+	       'faq_portlet::make_self_unavailable',
+	       'TCL'
+	);
+
+end;
+/
+show errors
+
+declare
+	foo integer;
+begin
+
+	-- Add the binding
+	acs_sc_binding.new (
+	    contract_name => 'portal_datasource',
+	    impl_name => 'faq_portlet'
+	);
 end;
 /
 show errors
