ce9f88440454ede7b90c64c26b9ae44722959bb9
Antonio Pisano <antonio@elettrotecnica.it>
2023-03-30 15:37:49 +0000

    Basic test of FAQ portlet rendering

--- tcl/test/faq-portlet-procs.tcl
+++ tcl/test/faq-portlet-procs.tcl
@@ -5,6 +5,45 @@ ad_library {
 
 }
 
+aa_register_case -procs {
+    faq_admin_portlet::show
+    faq_portlet::show
+} -cats {
+    api
+    smoke
+} render_portlet {
+    Test the rendering of the portlets
+} {
+    aa_run_with_teardown -rollback -test_code {
+        set package_id [site_node::instantiate_and_mount \
+                            -package_key faq \
+                            -node_name __test_faq_portlet]
+
+        set cf [list \
+                    package_id $package_id \
+                    shaded_p false \
+                   ]
+
+        foreach portlet {faq_admin_portlet faq_portlet} {
+            aa_section $portlet
+
+            set portlet [acs_sc::invoke \
+                             -contract portal_datasource \
+                             -operation Show \
+                             -impl $portlet \
+                             -call_args [list $cf]]
+
+            aa_log "Portlet returns: [ns_quotehtml $portlet]"
+
+            aa_false "No error was returned" {
+                [string first "Error in include template" $portlet] >= 0
+            }
+
+            aa_true "Portlet looks like HTML" [ad_looks_like_html_p $portlet]
+        }
+    }
+}
+
 aa_register_case -cats {web smoke} -libraries tclwebtest  tclwebtest_new_faq_portlet {
 
     Testing the creation a Faq from the portlet.
